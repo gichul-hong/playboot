9ca703cdbdc0e7eafdccc118436cba2b7d536073
홍기철 <hong-gichul@hong-gicheol-ui-MacBookAir.local>
2021-12-03 10:27:10 +0900

    adding the first module

---
+++ playdata/src/main/java/com/ong/playboot/playdata/PlaydataApplication.java
@@ -0,0 +1,13 @@
+package com.ong.playboot.playdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaydataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaydataApplication.class, args);
+    }
+
+}
---
+++ playdata/src/test/java/com/ong/playboot/playdata/PlaydataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ong.playboot.playdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaydataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
